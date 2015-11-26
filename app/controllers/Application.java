f95a577774ff016f84a21b46eef553635b88e199
yachironi <yachironi@hotmail.com>
2015-11-26 22:52:16 +0100

    commit test

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,6 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public Result index() {
+        // test - yasser
         return ok(index.render("Projet GL - ET5"));
     }
 
