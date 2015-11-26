03433a705863d9205763b82543aced45a2c2f36a
Amin Messaoudi <amin_messaoudi@yahoo.com>
2015-11-26 22:49:39 +0100

    commit local

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,6 +7,7 @@ import views.html.*;
 
 public class Application extends Controller {
 
+	// Allez Paris by Amin
     public Result index() {
         return ok(index.render("Projet GL - ET5"));
     }
