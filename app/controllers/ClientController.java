7c7a0d18c80cb7af8c456b642443e14c548d9b45
gishAkbaraly <gishan.akbarly@gmail.com>
2016-01-04 14:34:41 +0100

    Debut interface

---
+++ app/controllers/ClientController.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import models.Task;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.client;
+import views.html.tests;
+
+import static play.mvc.Results.ok;
+
+/**
+ * Created by Gishan on 03/01/2016.
+ */
+public class ClientController extends Controller {
+
+
+
+
+
+    public Result controllerResult() {
+        return ok(client.render());
+    }
+}
