60d796dddba8f7fca215228e0a2515792c649617
gishAkbaraly <gishan.akbarly@gmail.com>
2016-01-08 16:16:56 +0100

    epuration de l'interface

---
+++ app/controllers/DashboardController.java
@@ -0,0 +1,7 @@
+package controllers;
+
+/**
+ * Created by Gishan on 08/01/2016.
+ */
+public class DashboardController {
+}
---
+++ app/controllers/ProjetController.java
@@ -0,0 +1,7 @@
+package controllers;
+
+/**
+ * Created by Gishan on 07/01/2016.
+ */
+public class ProjetController {
+}
