d4b81180097c73913aaf6034a53f7b58152472ad
gishAkbaraly <gishan.akbarly@gmail.com>
2016-02-08 16:40:02 +0100

    JS pour les boutons oki

--- public/js/projet.js
+++ public/js/projet.js
@@ -1,3 +1,26 @@
 /**
  * Created by Gishan on 08/02/2016.
  */
+
+$(document).ready(function() {
+    $('.btn-group .btn').click(function (e) {
+        e.preventDefault();
+        var self = $(this),
+            controler = self.data('infos') || self.attr('data-infos');
+        $('.btn-group .btn').removeClass('btn-active');
+        $(this).addClass('btn-active');
+
+    });
+
+});
+
+$(document).ajaxComplete(function() {
+    $('.btn-group .btn').click(function (e) {
+        e.preventDefault();
+        var self = $(this),
+            controler = self.data('infos') || self.attr('data-infos');
+        $('.btn-group .btn').removeClass('btn-active');
+        $(this).addClass('btn-active');
+
+    });
+});
