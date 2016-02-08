0222e44a8e42065cbef323275c4418d74892a293
yachironi <yachironi@hotmail.com>
2016-02-08 14:51:34 +0100

    Fix sql insert bug

--- app/controllers/Global.java
+++ app/controllers/Global.java
@@ -1,11 +1,14 @@
 package controllers;
 
-import models.Projet;
-import play.*;
-import play.libs.*;
 import com.avaje.ebean.Ebean;
-import models.*;
-import java.util.*;
+import models.Client;
+import models.Utilisateur;
+import play.GlobalSettings;
+import play.Logger;
+import play.libs.Yaml;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Guillaume on 05/02/2016.
@@ -19,9 +22,20 @@ public class Global extends GlobalSettings {
         // Check if the database is empty
         if (Utilisateur.find.findRowCount() == 0) {
             //Ebean.save((List) Yaml.load("initial-data.yml"));
-            List<Object> all = (List<Object>)Yaml.load("initial-data.yml");
+            Map<String, List<Object>> all = (Map<String, List<Object>>) Yaml.load("initial-data.yml");
             System.out.println(all);
-            Ebean.save(all);
+
+            Ebean.save(all.get("adresses"));
+            Ebean.save(all.get("contacts"));
+            Ebean.save(all.get("clients"));
+            Ebean.save(all.get("utilisateurs"));
+            Ebean.save(all.get("projets"));
+            Ebean.save(all.get("taches"));
+
+
+            /*all.forEach((key, value) -> {
+                Ebean.save(value);
+            });*/
 
             Logger.debug(String.valueOf(Client.find.all()));
             Logger.debug(String.valueOf(Utilisateur.find.all()));
