939a7b74f25c2f2e4f70cf0fe78c17d0e0819e6a
gblanc <guillaume.blanchard37@gmail.com>
2016-02-11 00:09:46 +0100

    Date static method

--- app/models/Utils/Utils.java
+++ app/models/Utils/Utils.java
@@ -1,5 +1,7 @@
 package models.Utils;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -10,5 +12,13 @@ public class Utils {
     public static <T> boolean isEqualList(List<T> l1, List<T> l2){
         return (l1!=null && l2!= null && l1.containsAll(l2)
                 && l2.containsAll(l1) && l1.size()==l2.size());
+    }
+
+    public static Date getDateFrom(int year, int month, int day){
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.MONTH, month-1);
+        cal.set(Calendar.DAY_OF_MONTH, day);
+        return cal.getTime();
     }
 }
