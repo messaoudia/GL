876a3524e285ccad753b346033ded6e0bef4b34c
Amin Messaoudi <amin_messaoudi@yahoo.com>
2016-02-17 19:07:32 +0100

    topbar done
    A checker

--- public/js/menu.js
+++ public/js/menu.js
@@ -3,9 +3,14 @@
  */
 $(function(){
 
-    $("#sidebar-dashboard").click(function() {
+    $(".sidebar-btn-red").click(function() {
         $('.navbar').css("cssText", "background-color : #e74c3c !important;border-color : #e74c3c !important; transition: none;");
         $('.pear-theme').css("cssText", "background-color : #e74c3c !important;border-color : #e74c3c !important;");
+    });
+
+    $(".sidebar-btn-green").click(function() {
+        $('.navbar').css("cssText", "background-color : #1AB394 !important;border-color : #1AB394 !important; transition: none;");
+        $('.pear-theme').css("cssText", "background-color : #1AB394 !important;border-color : #1AB394 !important;");
     });
 });
 
