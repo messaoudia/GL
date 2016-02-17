0bed6969a64ec1b898aeabe4fc57aa887444a7e3
Amin Messaoudi <amin_messaoudi@yahoo.com>
2016-02-17 19:47:25 +0100

    [ERRATA] topbar done
    A checker

--- public/js/menu.js
+++ public/js/menu.js
@@ -4,17 +4,26 @@
 $(function(){
 
     $(".sidebar-btn-red").click(function() {
-        $('.navbar').css("cssText", "background-color : #e74c3c !important;border-color : #e74c3c !important; transition: none;");
-        $('.pear-theme').css("cssText", "background-color : #e74c3c !important;border-color : #e74c3c !important;");
+        changeTopBarRed();
     });
 
     $(".sidebar-btn-green").click(function() {
+        changeTopBarGreen();
+    });
+
+    function changeTopBarRed()
+    {
+        $('.navbar').css("cssText", "background-color : #e74c3c !important;border-color : #e74c3c !important; transition: none;");
+        $('.pear-theme').css("cssText", "background-color : #e74c3c !important;border-color : #e74c3c !important;");
+        var navHeader = document.getElementById('nav-header');
+        $(navHeader).addClass('nav-header-admin');
+    }
+
+    function changeTopBarGreen()
+    {
         $('.navbar').css("cssText", "background-color : #1AB394 !important;border-color : #1AB394 !important; transition: none;");
         $('.pear-theme').css("cssText", "background-color : #1AB394 !important;border-color : #1AB394 !important;");
-    });
+        var navHeader = document.getElementById('nav-header');
+        $(navHeader).removeClass('nav-header-admin');
+    }
 });
-
-function changeColorTopBar()
-{
-    var navbar = document.getElementsByClassName("pear-theme");
-}
