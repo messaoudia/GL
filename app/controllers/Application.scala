8b5e5756ade025e1957e478445cc89481d96040b
yachironi <yachironi@hotmail.com>
2016-02-19 23:48:51 +0100

    jdk 8

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,13 +1,12 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
 
   def index = Action {
     val javaVersion = System.getProperty("java.version")
-    Ok(views.html.index("Your new application is ready."+javaVersion))
+    Ok(views.html.index(javaVersion))
   }
 
 }
