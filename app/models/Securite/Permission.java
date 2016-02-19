ba4a08d06638fb6b7b2db0302b1e7470e6bd47a1
gblanc <guillaume.blanchard37@gmail.com>
2016-02-19 14:30:09 +0100

    List des permissions

--- app/models/Securite/Permission.java
+++ app/models/Securite/Permission.java
@@ -4,5 +4,42 @@ package models.Securite;
  * Created by yachironi on 05/02/16.
  */
 public enum Permission {
-    CreerProjet, ModifierProjet;
+
+    ModifierTousProjet,
+    SupprimerProjet,
+    AfficherTousProjet,
+    SupprimerTâche,
+    AfficherToutesTâche,
+    SupprContact,
+    SupprClient,
+    AfficherContact,
+    ModifierClient,
+    AfficherTousClient,
+    CreerUtilisateur,
+    ModifierUtilisateur,
+    SupprUtilisateur,
+    AfficherUtilisateur,
+    AfficherSideBarAdministrateur,
+    ImporrterClientContact,
+    ModifierProjetReponsable,
+    AfficherProjetResponsable,
+    CreerTâche,
+    ModifierTâche,
+    SupprimerTâcheNonCommencée,
+    AfficherTâcheResponsableProjet,
+    CreerContact,
+    CreerClient,
+    AfficherClientRespoProjet,
+    AfficherClientRespoTache,
+    AffichageTDBResponsableProjet, //ajout onglet + case projets en cours
+    AfficherSideBarResponsableProjet,
+    ExporterClientContact,
+    CreerProjet,
+    AfficherProjetTache,
+    ModifierAvancementTâche,
+    AfficherTâcheResponsable,
+    AffichageTDBCollaborateur,
+    AfficherSideBarCollaborateur,
+    AfficherEspaceUtilisateurResponsableProjet,
+    AfficherEspaceUtilisateurCollaborateur
 }
