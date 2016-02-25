301f86bb214ac00fafdb13d8becd829abe632bc7
zzcoolj <zzcooljay@gmail.com>
2016-02-25 12:27:56 +0100

    test creerTacheAuDessus

--- test/ModelManagerTest.java
+++ test/ModelManagerTest.java
@@ -37,9 +37,43 @@ public class ModelManagerTest {
                 assertEquals(tacheSelected.parent, null);
 
                 Projet projetSelected = Projet.find.byId(projet.id);
-                // Attention! assertTrue(projet.listTaches.contains(tacheSelected)); marche
-                //assertTrue(projet.listTaches.contains(tacheSelected));
-                assertTrue(projetSelected.listTaches.contains(tacheSelected));
+                assertTrue(projetSelected.listTaches().contains(tacheSelected));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    @Test
+    public void testCreerTacheAuDessus() {
+        running(fakeApplication(), ()-> {
+            //projet dateDebutTheorique: 2016,2,2, dateFinTheorique: 2016,2,10, dateDebutReel: 2016,2,3, dateFinReelTot: 2016,2,9, dateFinReelTard: 2016,2,9
+            Projet projet = new Projet("Site Apple","Développement du nouveau site d'Apple", null,
+                    Utils.getDateFrom(2016,2,2),Utils.getDateFrom(2016,2,10),Utils.getDateFrom(2016,2,3),
+                    Utils.getDateFrom(2016,2,9),Utils.getDateFrom(2016,2,9),24D, UniteProjetEnum.SEMAINE,new Byte("0"),false,false,null,3,null);
+            projet.save();
+
+            //tache dateDebut: (2016,2,3), dateFinTot: (2016,2,4), dateFinTard: (2016,2,4)
+            Tache tache1 = new Tache("Tache1","Cette tâche permet de réaliser l'étude du projet",0,true, Utils.getDateFrom(2016,2,3),
+                    Utils.getDateFrom(2016,2,4),Utils.getDateFrom(2016,2,4),20D,10D,20D,null,null);
+            tache1.save();
+
+            //tache dateDebut: (2016,2,3), dateFinTot: (2016,2,4), dateFinTard: (2016,2,4)
+            Tache tache2 = new Tache("Tache1","Cette tâche permet de réaliser l'étude du projet",0,true, Utils.getDateFrom(2016,2,3),
+                    Utils.getDateFrom(2016,2,4),Utils.getDateFrom(2016,2,4),20D,10D,20D,null,null);
+            tache2.save();
+
+            try {
+                projet.creerTacheInitialisationProjet(tache1);
+                projet.creerTacheAuDessus(tache2, tache1);
+
+                Tache tache2Selected = Tache.find.byId(tache2.id);
+                Projet projetSelected = Projet.find.byId(projet.id);
+                assertTrue(projetSelected.listTaches().contains(tache2Selected));
+
+                Logger.debug(tache1.idTache);
+                Logger.debug(tache2.idTache);
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
