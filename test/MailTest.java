89cb9febf4c88e708529671f3242aabdfeff2a5a
yachironi <yachironi@hotmail.com>
2016-03-01 21:17:21 +0100

    add mail test

---
+++ test/MailTest.java
@@ -0,0 +1,46 @@
+import com.avaje.ebean.Ebean;
+import org.apache.commons.io.FileUtils;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import play.test.FakeApplication;
+import play.test.Helpers;
+
+import java.io.IOException;
+
+/**
+ * Created by yachironi on 01/03/16.
+ */
+public class MailTest {
+
+    public static FakeApplication app;
+    public static String createDdl = "";
+    public static String dropDdl = "";
+
+    @BeforeClass
+    public static void startApp() throws IOException {
+        app = Helpers.fakeApplication(Helpers.inMemoryDatabase());
+        Helpers.start(app);
+
+        // Reading the evolution file
+        String evolutionContent = FileUtils.readFileToString(
+                app.getWrappedApplication().getFile("conf/evolutions/default/1.sql"));
+        String[] splittedEvolutionContent = evolutionContent.split("# --- !Ups");
+        String[] upsDowns = splittedEvolutionContent[1].split("# --- !Downs");
+        createDdl = upsDowns[0];
+        dropDdl = upsDowns[1];
+    }
+
+    @Before
+    public void beforeEachTest() {
+        Ebean.execute(Ebean.createCallableSql(dropDdl));
+        Ebean.execute(Ebean.createCallableSql(createDdl));
+    }
+
+    @AfterClass
+    public static void stopApp() {
+        Helpers.stop(app);
+    }
+
+
+}
