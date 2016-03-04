119fef6896a3fae406efff9a8fba9dc8d345ca95
yachironi <yachironi@hotmail.com>
2016-03-04 15:30:36 +0100

    Add notification sending on pagechange, on window close, on logout

--- app/controllers/NotificationController.java
+++ app/controllers/NotificationController.java
@@ -16,15 +16,16 @@ public class NotificationController extends Controller {
 
     /**
      * Utilisée dans les pages 'Dashboard', 'Projet' et 'Client' d'un collaborateur
+     *
      * @param idTache
      * @param idUser
      * @return
      */
-    public Result activerOuDesactiverNotificationTache(long idTache, long idUser){
+    public Result activerOuDesactiverNotificationTache(long idTache, long idUser) {
         Tache tache = Tache.find.byId((idTache));
         Utilisateur user = Utilisateur.find.byId((idUser));
 
-        if(user.hasActiverNotification(tache)){
+        if (user.hasActiverNotification(tache)) {
             user.desactiverNotification(tache);
         } else {
             user.activerNotification(tache);
@@ -32,10 +33,10 @@ public class NotificationController extends Controller {
         return ok();
     }
 
-    public Result clickNotificationsLues(long idUser, String listIdNotif){
+    public Result clickNotificationsLues(long idUser, String listIdNotif) {
         Utilisateur user = Utilisateur.find.byId(idUser);
         String[] listIdNotifParse = listIdNotif.split(",");
-        for(String idNotifStr : listIdNotifParse){
+        for (String idNotifStr : listIdNotifParse) {
             long idNotif = Long.parseLong(idNotifStr);
             Notification notif = Notification.find.byId(idNotif);
             notif.etatLecture = true;
@@ -46,13 +47,13 @@ public class NotificationController extends Controller {
         return ok(user.nbNotificationsNonLues() + "");
     }
 
-    public Result clickNotificationsSupprimer(long idUser, String listIdNotif){
+    public Result clickNotificationsSupprimer(long idUser, String listIdNotif) {
         Utilisateur user = Utilisateur.find.byId(idUser);
         String[] listIdNotifParse = listIdNotif.split(",");
-        for(String idNotifStr : listIdNotifParse){
+        for (String idNotifStr : listIdNotifParse) {
             long idNotif = Long.parseLong(idNotifStr);
             Notification notif = Notification.find.byId(idNotif);
-            if(user.listNotifications.contains(notif)){
+            if (user.listNotifications.contains(notif)) {
                 user.listNotifications.remove(notif);
                 Notification.find.deleteById(notif.id);
                 user.update();
@@ -63,9 +64,22 @@ public class NotificationController extends Controller {
         return ok(user.nbNotificationsNonLues() + "");
     }
 
-    public Result sendNotifications(){
-        Logger.debug(request().body().asText());
-        return ok(request().body().asText());
+    public Result sendNotifications() {
+
+        Logger.debug("Envoie notification...");
+        Logger.debug("Utilisateur: "+Login.getUtilisateurConnecte());
+
+        //Login.getUtilisateurConnecte().sendNotifications();
+
+        //if (request().body() != null) {
+        //    Logger.error("Doeing the notification parsing...");
+        //    Logger.debug("Notifications: " + request().body().asJson());
+        //    return ok(request().body().asJson());
+        //} else {
+        //    Logger.error("Body of request is null");
+        //    return badRequest(Json.toJson(ImmutableMap.of("error", "body is null")));
+        //}
+        return ok();
     }
 
 }
